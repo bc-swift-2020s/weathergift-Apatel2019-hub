fb3f4b00b93e7f82a38faec1225815c43f6f4408
Aashay Patel <patelol@bc.edu>
2020-03-08 22:32:29 -0400

    Completed 7.2

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Aashay Patel on 3/5/20.
+//  Copyright © 2020 Aashay Patel. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
