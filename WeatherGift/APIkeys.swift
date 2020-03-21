83baca10aa8fee88527e60a7e38431c9f45edc8f
Aashay Patel <patelol@bc.edu>
2020-03-21 12:28:56 +0400

    Completed 7.5

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Aashay Patel on 3/21/20.
+//  Copyright © 2020 Aashay Patel. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyAgBCFpsJq5xh02gVpzlN01MVRugWPEGeQ"
+}
