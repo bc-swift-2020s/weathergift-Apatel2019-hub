306fbde92b5367a05899c8b33b796fcb43f787d4
Aashay Patel <patelol@bc.edu>
2020-03-23 10:41:37 +0400

    Completed 7.10

--- WeatherGift/APIkeys.swift
+++ WeatherGift/APIkeys.swift
@@ -10,4 +10,9 @@ import Foundation
 
 struct APIkeys {
     static let googlePlacesKey = "AIzaSyAgBCFpsJq5xh02gVpzlN01MVRugWPEGeQ"
+    static let darkSkyKey = "e035763a00dc88d877b0c4421f8c7bd7"
+}
+
+struct APIurls {
+    static let darkSkyURL = "https://api.darksky.net/forecast/"
 }
